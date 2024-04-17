01686595ba725e2c6fd66cc97c06a061ce8a8beb
SamkamDev <57526446+samuelkamotho92@users.noreply.github.com>
2024-04-17 13:13:51 +0300

    Update msal.ts

--- app/component/AuthProvider/msal.ts
+++ app/component/AuthProvider/msal.ts
@@ -2,8 +2,8 @@ import * as msal from "@azure/msal-browser";
 
 const msalConfig: msal.Configuration = {
     auth: {
-        clientId: "314aad1d-bd2d-45a3-b6b5-5281204eb541", // This is the ONLY mandatory field that you need to supply.
-        authority: "https://login.microsoftonline.com/thh-llc.com ",
+        clientId: "", // This is the ONLY mandatory field that you need to supply.
+        authority: " ",
         redirectUri: 'http://localhost:5173', // Points to window.location.origin. You must register this URI on Azure Portal/App Registration.
         // postLogoutRedirectUri: 'http://localhost:3000', // Indicates the page to navigate after logout.
         navigateToLoginRequestUrl: true, // If "true", will navigate back to the original request location before processing the auth code response.
